9bcac78075ae9c62efb949039da791fc10d0385e
Viitiiok <victor76167@gmail.com>
2024-07-16 15:31:25 +0300

    Modules/Controllers/Providers/Docker/Prisma

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Body, Post } from "@nestjs/common";
+import { Request } from "express";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthController {
+   constructor(private authService: AuthService) {}
+
+   @Post('signup')
+   signup(@Body() dto: AuthDto) {
+      console.log({
+      dto,
+      });
+   }
+
+   @Post('signin')
+   signin() {
+      return this.authService.signin();
+   }
+}
